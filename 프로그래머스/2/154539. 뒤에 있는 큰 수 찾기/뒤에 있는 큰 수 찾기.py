b84a9b25968e0c1a11f7347e90c6fea62fe6d34a
전쥰 <90656206+j1y4own@users.noreply.github.com>
2024-01-22 23:39:07 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 477.88 ms, Memory: 181 MB -BaekjoonHub

---
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.py
@@ -0,0 +1,11 @@
+from heapq import heappop,heappush
+
+def solution(numbers):
+    answer = [-1 for _ in range(len(numbers))]
+    heap = [(numbers[0],0)]
+    for i,n in enumerate(numbers[1:]):
+        while heap and heap[0][0]<n:
+            _,idx = heappop(heap)
+            answer[idx] = n
+        heappush(heap,(n,i+1))
+    return answer
